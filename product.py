bd36be0c760997a2ec6ec36e88185a52c365de13
Maksim Fotin <maksimfotin144@gmail.com>
2024-03-31 21:13:34 +0700

    add ReprMixin, create abstract class Order

--- product.py
+++ product.py
@@ -1,27 +1,50 @@
-class Product:
+from abc import ABC, abstractmethod
+
+
+class ReprMixin:
+    def __init__(self):
+        print(repr(self))
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        return (f"Был создан объект: {class_name}\n"
+                f"Атрибуты: {self.__dict__.items()}")
+
+
+class Order(ABC):
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
 
+    @abstractmethod
+    def display_info(self):
+        pass
+
+
+class Product(Order, ReprMixin):
+
+    def display_info(self):
+        return f"Имя продукта: {self.name}, Цена: {self.__price}, Количество: {self.quantity}"
+
     @staticmethod
     def create_product(name, price, quantity):
-        new_product = Product(name, price, quantity)
-        if isinstance(new_product, Product):
-            return new_product
-        else:
-            raise TypeError("Можно создавать только объекты класса Product или его наследников")
+        return Product(name, price, quantity)
 
 
 class Smartphone(Product):
     def __init__(self, name: str, description: str, price: float, quantity: int,
-                 performance: int, model: str, memory: float, color: str):
+                 performance: float, model: str, memory: float, color: str):
         super().__init__(name, description, price, quantity)
         self.performance = performance
         self.model = model
         self.memory = memory
         self.color = color
+
+    def display_info(self):
+        return (f"Имя смартфона: {self.name}, Описание: {self.description} Цена: {self.__price}, /n "
+                f"Количество: {self.quantity}, Модель: {self.model}, Память: {self.memory}, Цвет: {self.color}")
 
 
 class LawnGrass(Product):
@@ -32,13 +55,18 @@ class LawnGrass(Product):
         self.germination_period = germination_period
         self.color = color
 
+    def display_info(self):
+        return (f"Имя травы: {self.name}, Описание: {self.description} Цена: {self.__price}, /n "
+                f"Количество: {self.quantity}, Страна: {self.country}, Период роста: {self.germination_period}, /n "
+                f"Цвет: {self.color}")
+
     @property
     def price(self):
         return self.__price
 
     @price.setter
-    def price(self, __price):
-        if __price <= 0:
+    def price(self, price):
+        if price <= 0:
             print("Ошибка: Цена введена некорректно. Цена должна быть больше нуля.")
 
     def __str__(self):
--- test/test_product.py
+++ test/test_product.py
@@ -1,5 +1,5 @@
 import pytest
-from product import Product, Smartphone, LawnGrass
+from product import Product, Smartphone, LawnGrass, Order
 
 
 # Тест на создание экземпляра класса Product
@@ -9,30 +9,11 @@ def test_smartphone_inheritance():
     assert isinstance(phone, Product)
 
 
-def test_lawn_grass_creation():
-    lawn_grass = LawnGrass("Lawn Grass", "Описание", 20, 100, "USA",
-                           "Месяц", "Зеленый")
-    assert lawn_grass.name == "Lawn Grass"
-    assert lawn_grass.description == "Описание"
-    assert lawn_grass._Product__price == 20
-    assert lawn_grass.quantity == 100
-    assert lawn_grass.country == "USA"
-    assert lawn_grass.germination_period == "Месяц"
-    assert lawn_grass.color == "Зеленый"
-
-
-def test_product_creation():
-    product = Product("Продукт", "Описание", 100, 10)
-    assert product.name == "Продукт"
-    assert product.description == "Описание"
-    assert product._Product__price == 100
-    assert product.quantity == 10
-
-
 def test_product_addition_different_class():
     product1 = Product("Product1", "Описание", 100, 10)
     product2 = LawnGrass("Grass1", "Описание", 150, 15, "USA",
                          "Месяц", "Зеленый")
     with pytest.raises(TypeError):
         result = product1 + product2
+
 
